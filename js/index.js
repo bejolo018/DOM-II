f1da5de9b80d79a0216942c22a643f7600c9ebef
unknown <bejolo018@gmail.com>
2019-04-09 15:24:49 -0500

    built basic foundation of event foundation and completed two events

--- js/index.js
+++ js/index.js
@@ -1,1 +1,82 @@
 // Your code goes here
+
+
+//Mouseover
+
+const mouseOver = document.querySelector('.btn');
+
+mouseOver.addEventListener('mouseover', function(eventObject){
+    console.log('mouse over!');
+});
+
+// //Keydown
+
+// const keyDown = document.querySelector('.main-navigation');
+
+// keyDown.addEventListener('keydown', function(event){
+//     console.log('keydown!')
+// });
+
+// //Wheel
+
+// const wheel = document.querySelector('');
+
+// wheel.addEventListener('wheel', function(event){
+//     console.log('wheel!')
+// });
+
+// //Drag/drop
+
+// const dragDrop = document.querySelector('');
+
+// dragDrop.addEventListener('dragdrop', function(event){
+//     console.log('dragdrop!')
+// });
+
+// //Load
+
+// const load = document.querySelector('');
+
+// load.addEventListener('load', function(event){
+//     console.log('load!')
+// });
+
+// //Focus
+
+// const focus = document.querySelector('');
+
+// focus.addEventListener('focus', function(event){
+//     console.log('focus!')
+// });
+
+// //Resize
+
+// const resize = document.querySelector('');
+
+// resize.addEventListener('resize', function(event){
+//     console.log('resize!')
+// });
+
+// //Scroll
+
+// const scroll = document.querySelector('');
+
+// scroll.addEventListener('scroll', function(event){
+//     console.log('scroll!')
+// });
+
+// //Select
+
+// const select = document.querySelector('');
+
+// select.addEventListener('select', function(event){
+//     console.log('select!')
+// });
+
+//DoubleClick
+
+const dblclick = document.querySelector('.btn');
+
+dblclick.addEventListener('dblclick', function(event){
+    console.log('dblclick!')
+});
