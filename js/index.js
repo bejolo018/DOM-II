5b6ad1d20ab3f0810b54f1870d6f8403c5771c13
unknown <bejolo018@gmail.com>
2019-04-09 17:38:10 -0500

    completed all aesthetic difference changes for all but drag/drop

--- js/index.js
+++ js/index.js
@@ -11,9 +11,11 @@ mouseOver.addEventListener('mouseover', function(eventObject){
 
 //Keydown X
 
-// window.addEventListener('keydown', function(event){
-//     window.style.color = 'green';
-// });
+const hide = document.querySelector('.content-section')
+
+window.addEventListener('keydown', function(event){
+    hide.style.background = 'black';
+});
 
 //Wheel XX
 const wheel = document.querySelector('.btn');
@@ -28,30 +30,34 @@ wheel.addEventListener('wheel', function(event){
 //     console.log('dragdrop!')
 // });
 
-//Load X
+//Load XX
+
+const color = document.querySelector('.container')
 
 window.addEventListener('load', function(event){
-    console.log('load!')
+    color.style.background = 'whitesmoke'
 });
 
-//Focus X
+//Focus XX
 
 window.addEventListener('focus', function(event){
-    console.log('focus!')
+    window.alert('refresh please!')
 });
 
 //Resize X
 
-// window.addEventListener('resize', function(event){
-//     window.style.background = 'blue';
-// });
 
-//Scroll X
 
-const scroll = document.querySelector('.container-home');
+window.addEventListener('resize', function(event){
+    hide.style.background = 'white'
+});
+
+//Scroll XX
+
+const scroll = document.querySelector('.btn');
 
 scroll.addEventListener('scroll', function(event){
-    scroll.style.background = 'purple';
+   scroll.style.background = 'purple'
 });
 
 //Select X
